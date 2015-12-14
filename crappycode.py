9c4ac0753441b31cd279a6153cf3a51c8364a3ee
HHStudent <dimitrisomoff@hanovernorwichschools.org>
2015-12-14 14:37:30 -0500

    Updated from Brython Server: 12/14/2015 2:37:30 PM

--- crappycode.py
+++ crappycode.py
@@ -2,4 +2,25 @@
 Name: Dimitri Somoff
 Final Project: Flappy bird remake "Crappy Bird"
 """
-import ggame
+from ggame import App, Color, LineStyle, Sprite, ImageAsset, Frame
+
+"""class CrappyBird(Sprite):
+    
+    def __init__(self, x, y):
+        super().__init__(x, y)
+        
+"""
+
+class CrappyApp(App):
+    background = ImageAsset("images/background.png")
+    
+    def __init__(self, width, height):
+        super().__init__(width, height)
+        bg = Sprite(background, (0,0))
+        
+
+
+
+
+myapp = CrappyApp(400, 708)
+myapp.run()
