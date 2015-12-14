f3fea64209fe4e9232a48c59085e851f17943b36
HHStudent <dimitrisomoff@hanovernorwichschools.org>
2015-12-14 14:49:19 -0500

    Updated from Brython Server: 12/14/2015 2:49:19 PM

--- crappycode.py
+++ crappycode.py
@@ -4,19 +4,23 @@ Final Project: Flappy bird remake "Crappy Bird"
 """
 from ggame import App, Color, LineStyle, Sprite, ImageAsset, Frame
 
-"""class CrappyBird(Sprite):
+class Bird(Sprite):
+    asset1 = ImageAsset("images/0.png")
+    asset2 = ImageAsset("images/1.png")
+    asset3 = ImageAsset("images/2.png")
     
-    def __init__(self, x, y):
-        super().__init__(x, y)
+    def __init__(self, position):
+        super().__init__(Bird.asset1, position)
+        self.vy = y
         
-"""
+
 
 class CrappyApp(App):
-    background = ImageAsset("images/background.png")
-    
     def __init__(self, width, height):
         super().__init__(width, height)
-        bg = Sprite(background, (0,0))
+        bg_asset = ImageAsset("images/background.png")
+        bg = Sprite(bg_asset, (0,0))
+        Bird((0, 350))
         
 
 
