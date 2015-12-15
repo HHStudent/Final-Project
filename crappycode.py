35f12197f2b99570100112db4ed452def59602f3
HHStudent <dimitrisomoff@hanovernorwichschools.org>
2015-12-15 14:40:17 -0500

    Updated from Brython Server: 12/15/2015 2:40:18 PM

--- crappycode.py
+++ crappycode.py
@@ -5,23 +5,24 @@ Final Project: Flappy bird remake "Crappy Bird"
 from ggame import App, Color, LineStyle, Sprite, ImageAsset, Frame
 
 class Bird(Sprite):
-    images = [((ImageAsset("images/0.png")), (ImageAsset("images/1.png")), (ImageAsset("images/2.png")))]
     asset1 = ImageAsset("images/0.png")
     asset2 = ImageAsset("images/1.png")
     asset3 = ImageAsset("images/2.png")
     
     def __init__(self, position):
-        super().__init__(Bird.images[0], position)
+        super().__init__(Bird.asset1, position)
         self.birdy = 350
+        self.gravity = 5
         CrappyApp.listenKeyEvent("keydown", "space", self.Jump)
 
     def step(self):
+        self.gravity += .2
+        self.birdy += self.gravity
         self.y = self.birdy
 
     def Jump(self, event):
-        self.birdy -= 5
-        self.SetImage(Bird.asset2)
-
+        self.birdy -= 15
+        self.gravity = .1
 
 class CrappyApp(App):
     def __init__(self, width, height):
