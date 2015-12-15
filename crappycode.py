ae327cd0dba4f56755259feb6ffb2997953d092e
HHStudent <dimitrisomoff@hanovernorwichschools.org>
2015-12-15 18:55:20 -0500

    Updated from Brython Server: 12/15/2015 6:55:20 PM

--- crappycode.py
+++ crappycode.py
@@ -23,13 +23,12 @@ class Bird(Sprite):
         self.gravity += .05
         self.birdy += self.gravity
         self.y = self.birdy
-        if self.walx == 400:
-            TopWall((self.wallx, self.wally))
-        """
         TopWall((self.wallx, self.wally))
-        #BottomWall((self.wallx, bottomwally))
         self.wallx -= 5
-"""
+        #TopWall((self.wallx, self.wally))
+        #BottomWall((self.wallx, bottomwally))
+        #self.wallx -= 5
+
     def Jump(self, event):
         self.birdy -= 20
         self.gravity = .1
