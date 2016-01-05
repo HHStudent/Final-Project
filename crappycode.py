034fce2f334a02789e07262f022d1c8ee47e7203
HHStudent <dimitrisomoff@hanovernorwichschools.org>
2016-01-05 17:00:42 -0500

    Updated from Brython Server: 1/5/2016 5:00:40 PM

--- crappycode.py
+++ crappycode.py
@@ -93,9 +93,9 @@ class Bird(Sprite):
                 self.die()
             if len(collides1):
                 self.die()
-        #if self.visible == True:
-        #    if self.y <= -30:
-        #        self.die()
+        if self.visible == True:
+            if self.y <= -30:
+                self.die()
         #    if self.y >= 708:
         #        self.die()
 
